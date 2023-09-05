d45ad5037b5c4f013d2deb64e75200c3059213d2
cscodeabhishek <134864196+cscodeabhishek@users.noreply.github.com>
2023-09-05 02:12:15 +0000

    variables and datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,12 @@
+const accountId= 1234
+let accountEmail= "test@gmail.com"
+var password= "12345"
+accountCity="delhi"
+// accountId=2
+accountEmail="test2@gmail.com"
+password="abhhh"
+accountCity="bengaluruu"
+console.log(accountId);
+console.log(accountEmail);
+console.log(password);
+console.table([accountId,accountEmail,password,accountCity])
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,3 @@
+"use strcit";// treat all js code as newer version
+// alert(3+3) this is not allowed in the node js
+console.log(2+4)
